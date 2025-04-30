ba0b8df6fab33c231c11ff018bff4bb2da5b5e34
a.safronov <a.safronov@propellerads.net>
2025-04-30 11:02:14 +0300

    8 step

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -9,14 +9,16 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.print("Please enter the game number and press Enter.\n" +
-                "1 - Greet\n" +
-                "2 - Even\n" +
-                "3 - Calc\n" +
-                "4 - GCD\n" +
-                "5 - Progression\n" +
-                "0 - Exit" +
-                "\n- ");
+        System.out.print("""
+                Please enter the game number and press Enter.
+                1 - Greet
+                2 - Even
+                3 - Calc
+                4 - GCD
+                5 - Progression
+                0 - Exit\
+        
+                -\s""");
         Scanner s = new Scanner(System.in);
         int choice = s.nextInt();
         switch (choice) {
